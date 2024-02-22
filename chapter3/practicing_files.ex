2792ad984ff4a52cad574ff228efd7546a071649
Gianluca Rea <reagianluca97@gmail.com>
2024-02-22 14:52:12 +0100

    Chapter 3 Done!

--- chapter3/practicing_files.ex
+++ chapter3/practicing_files.ex
@@ -29,8 +29,22 @@ defmodule FileRocket do
     end)
   end
 
+  def longest_line!(path) do
+    x = FileRocket.longest_line_lenght!(path)
+    File.stream!(path)
+    |> Stream.map(&String.replace(&1,"\n",""))
+    |> Enum.filter(&(String.length(&1) === x))
+  end
+
+  def words_per_line!(path) do
+    File.stream!(path)
+    |> Stream.map(&String.replace(&1,"\n",""))
+    |> Enum.map(&(length(String.split(&1))))
+  end
 end
 
 FileRocket.large_lines!("./prova.txt") |> IO.inspect()
 FileRocket.lines_lengths!("./prova.txt") |> IO.inspect()
 FileRocket.longest_line_lenght!("./prova.txt") |> IO.inspect()
+FileRocket.longest_line!("./prova.txt") |> IO.inspect()
+FileRocket.words_per_line!("./prova.txt") |> IO.inspect()
