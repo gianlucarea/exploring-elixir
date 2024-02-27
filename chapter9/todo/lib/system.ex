5af89b518549ee439384846c905bbc5c02e3b7ad
Gianluca Rea <reagianluca97@gmail.com>
2024-02-27 13:04:59 +0100

    chapter9 - supervise db

---
+++ chapter9/todo/lib/system.ex
@@ -0,0 +1,11 @@
+defmodule Todo.System do
+  use Supervisor
+
+  def start_link do
+    Supervisor.start_link([Todo.Database, Todo.Cache], strategy: :one_for_one)
+  end
+
+  def init(_) do
+  end
+
+end
