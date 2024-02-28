d9b86062764b73622001f7177d6537aff6141764
Gianluca Rea <reagianluca97@gmail.com>
2024-02-28 15:39:37 +0100

    chapter12

--- chapter12/todo/lib/server.ex
+++ chapter12/todo/lib/server.ex
@@ -1,15 +1,22 @@
 defmodule Todo.Server do
   use GenServer, restart: :temporary
 
-  @expiry_idle_timeout :timer.seconds(10)
+  @expiry_idle_timeout :timer.seconds(10000)
 
 
   def start_link(name) do
-    GenServer.start_link(__MODULE__, name, name: via_tuple(name))
+    GenServer.start_link(__MODULE__, name, name: global_name(name))
   end
 
-  defp via_tuple(name) do
-    Todo.ProcessRegistry.via_tuple({__MODULE__, name})
+  defp global_name(name) do
+    {:global,{__MODULE__, name}}
+  end
+
+  def whereis(name) do
+    case :global.whereis_name(name) do
+      :undefined -> nil
+      pid -> pid
+    end
   end
 
   def add_entry(todo_server, new_entry) do
